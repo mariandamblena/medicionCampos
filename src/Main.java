5e0f24982153d34a3ccfd5d95ad9ce4bed6875fc
mariandamblena <marianodambo@hotmail.com>
2025-06-21 11:13:00 -0300

    Probando la implementacion del pibe

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,36 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import implementacion.ArbolPrecipitaciones;
+import implementacion.DiccionarioSimple;
+import implementacion.Conjunto;
+import tdas.ABBPrecipitacionesTDA;
+import tdas.ConjuntoTDA;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Crear el árbol de precipitaciones
+        ABBPrecipitacionesTDA arbol = new ArbolPrecipitaciones();
+        arbol.inicializar();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Agregar campo
+        arbol.agregar("CampoA");
+
+        // Agregar precipitaciones al campo (año y mes separados como String)
+        arbol.agregarMedicion("CampoA", "2024", "06", 12, 10); // 10 mm el 12 de junio 2024
+        arbol.agregarMedicion("CampoA", "2024", "06", 13, 5);  // 5 mm el 13 de junio
+        arbol.agregarMedicion("CampoA", "2024", "06", 12, 3);  // suma 3 mm más al 12
+
+        // Simular un diccionario directamente
+        DiccionarioSimple dic = new DiccionarioSimple();
+        dic.inicializar();
+        dic.agregar(1, 100);
+        dic.agregar(2, 200);
+        dic.agregar(1, 50); // debería sumar a la clave 1
+
+        ConjuntoTDA claves = dic.obtenerClaves();
+        while (!claves.estaVacio()) {
+            int k = claves.elegir();
+            claves.sacar(k);
+            int v = dic.recuperar(k);
+            System.out.println("Día: " + k + " -> " + v + " mm");
         }
     }
 }
